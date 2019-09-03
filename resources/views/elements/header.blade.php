d935b041d59baeca5e18ee28c2bbcd7ab34f1214
datnt26 <datnt.2507@gmail.com>
2019-09-03 11:20:37 +0700

    login & logout function

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -1,1 +1,28 @@
 
+@if (Auth::user())
+    <div class="container-fluid">
+      	<div class="row">
+         	<nav class="navbar navbar-default">
+             	<div class="container-fluid">
+                  	<div class="navbar-header">
+                     
+                  	</div>
+                  	<ul class="nav navbar-nav navbar-right">
+	                    <li>
+	                    	<a href="{{route('home')}}">Home</a>
+		                </li>
+		                <li><a href="javascript:void(0)">Profile</a></li>
+	                  	<li>
+	                    	<a  href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+	                  	</li>
+                  	</ul>
+             	</div>
+         	</nav>
+      </div>
+    <div>
+@endif
